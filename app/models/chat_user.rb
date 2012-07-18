3224626e5aa20b24d72e0b8a9a7420ffa7857265
Alexi Carey <ciegoproductions@gmail.com>
2012-07-18 13:48:17 -0600

    chat user update logic to allow existing chatnodes to update when new data

--- app/models/chat_user.rb
+++ app/models/chat_user.rb
@@ -1,27 +1,32 @@
 class ChatUser < ActiveRecord::Base
   attr_accessible :avatar_link, :first_name, :last_name, :uid, :user_id, :user_node, :username
 
+  #has_many :chat_messages
+
   # This is called to handle user verification requests that return response parameters
   def self.create_user_by_params(user_json, user_node)
 
-    username = !user_json.empty? ? user_json.fetch("username") : nil
+    username = !user_json.nil? ? user_json.fetch("username") : nil
 
-    users = ChatUser.where(:user_node => user_node).where(:username => username)
+    users = ChatUser.where(:user_node => user_node).where(:username => username).limit(1)
     if users.empty?
       @user = ChatUser.new
       @user.user_node = user_node
-
-      if !user_json.empty?
-        @user.username = user_json.fetch("username")
-        @user.user_id = user_json.fetch("id")
-        @user.first_name = user_json.fetch("firstName")
-        @user.last_name = user_json.fetch("lastName")
-        @user.uid = user_json.fetch("fbId")
-        @user.avatar_url = user_json.fetch("avatarUrl")
-      end
-
-      @user.save
-      return @user
+    else
+      @user = users.first
     end
+
+    if !user_json.nil?
+      @user.username = user_json.fetch("username")
+      @user.user_id = user_json.fetch("id")
+      @user.first_name = user_json.fetch("firstName")
+      @user.last_name = user_json.fetch("lastName")
+      @user.uid = user_json.fetch("fbId")
+      @user.avatar_url = user_json.fetch("avatarUrl")
+    end
+
+    @user.save
+    return @user
+
   end
 end
